270fbd2274d42d35cdc25d2c71b4f88c71646947
Infaredofficially <130591540+Infaredofficially@users.noreply.github.com>
2023-09-14 08:35:23 +0100

    Update batch.py

--- main/plugins/batch.py
+++ main/plugins/batch.py
@@ -31,7 +31,7 @@ async def cancel(event):
     if not event.sender_id in batch:
         return await event.reply("No batch active.")
     batch.clear()
-    await event.reply("Done.")
+    await event.reply("Done, Senpai.")
     
 @Drone.on(events.NewMessage(incoming=True, from_users=AUTH, pattern='/batch'))
 async def _batch(event):
@@ -42,32 +42,32 @@ async def _batch(event):
         await event.reply(r)
         return       
     if event.sender_id in batch:
-        return await event.reply("You've already started one batch, wait for it to complete you dumbfuck owner!")
+        return await event.reply("You've already started one batch, chotto matte owner Senpai!")
     async with Drone.conversation(event.chat_id) as conv: 
         if s != True:
-            await conv.send_message("Send me the message link you want to start saving from, as a reply to this message.", buttons=Button.force_reply())
+            await conv.send_message("Send me the message link you want to start saving from, as a reply to this message, Senpai🤭.", buttons=Button.force_reply())
             try:
                 link = await conv.get_reply()
                 try:
                     _link = get_link(link.text)
                 except Exception:
-                    await conv.send_message("No link found.")
+                    await conv.send_message("No link found, baka😌.")
                     return conv.cancel()
             except Exception as e:
                 print(e)
-                await conv.send_message("Cannot wait more longer for your response!")
+                await conv.send_message("Cannot wait more longer for your response 🥵!")
                 return conv.cancel()
-            await conv.send_message("Send me the number of files/range you want to save from the given message, as a reply to this message.", buttons=Button.force_reply())
+            await conv.send_message("Send me the number of files/range you want to save from the given message, as a reply to this message, Senpai😊.", buttons=Button.force_reply())
             try:
                 _range = await conv.get_reply()
             except Exception as e:
                 print(e)
-                await conv.send_message("Cannot wait more longer for your response!")
+                await conv.send_message("Cannot wait more longer for your response 🥵!")
                 return conv.cancel()
             try:
                 value = int(_range.text)
                 if value > 100:
-                    await conv.send_message("You can only get upto 100 files in a single batch.")
+                    await conv.send_message("You can only get upto 100 files in a single batch, Baka 😌.")
                     return conv.cancel()
             except ValueError:
                 await conv.send_message("Range must be an integer!")
